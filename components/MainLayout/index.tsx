0b4193bbc1b009d97e38931e757fd8618a565065
H-ymt <y.handai1272@gmail.com>
2024-03-07 21:50:32 +0900

    feat(layout):

---
+++ components/MainLayout/index.tsx
@@ -0,0 +1,13 @@
+import Aside from "../Aside"
+import styels from "./index.module.scss"
+
+export default function MainLayout({
+  children,
+}: { children: React.ReactNode }) {
+  return (
+    <div className={styels.layout}>
+      <Aside />
+      <main>{children}</main>
+    </div>
+  )
+}
