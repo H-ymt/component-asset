4367e8ba9df29295246b9424264993ee96317270
H-ymt <y.handai1272@gmail.com>
2024-03-07 21:47:32 +0900

    feat(Added menu categories constants):

---
+++ constants/index.ts
@@ -0,0 +1,15 @@
+interface CategoriesProps {
+  categorie: string
+  slug: string
+}
+
+export const CATEGORIES = [
+  {
+    categorie: "Accordion",
+    slug: "accordion",
+  },
+  {
+    categorie: "Menu",
+    slug: "menu",
+  },
+]
