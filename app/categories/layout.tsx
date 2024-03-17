c25a13d7d2b21d40d9733fb7eb4d028eeafb61d1
H-ymt <y.handai1272@gmail.com>
2024-03-17 16:59:08 +0900

    feat: Delete

--- app/categories/layout.tsx
+++
@@ -1,7 +0,0 @@
-import styles from "./layout.module.scss"
-
-export default function CategoriesLayout({
-  children,
-}: { children: React.ReactNode }) {
-  return <div className={styles.layout}>{children}</div>
-}
