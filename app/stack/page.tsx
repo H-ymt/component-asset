68f06f0a4ef0517ee9bb2778d2467d6a45170856
H-ymt <y.handai1272@gmail.com>
2024-03-17 17:01:28 +0900

    feat(stack): Implementing stack component

--- app/stack/page.tsx
+++ app/stack/page.tsx
@@ -1,29 +1,57 @@
 import Container from "@/components/Container"
+import Biome from "@/components/Icons/Biome"
+import NextJs from "@/components/Icons/Next"
+import ShadcnUi from "@/components/Icons/ShadcnUi"
+import Stylelint from "@/components/Icons/Stylelint"
+import Vercel from "@/components/Icons/Vercel"
 import styles from "./page.module.scss"
 
 export default function Page() {
   return (
     <Container>
       <section>
-        <h1>Used by</h1>
+        <h1 className={styles.sectionTitle}>Used by</h1>
         <div className={styles.container}>
-          <a href="https//" className={styles.stackCard}>
-            Vercel
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <NextJs />
           </a>
-          <a href="https//" className={styles.stackCard}>
-            Vercel
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <Vercel />
           </a>
-          <a href="https//" className={styles.stackCard}>
-            Vercel
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <Vercel />
           </a>
-          <a href="https//" className={styles.stackCard}>
-            Vercel
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <Vercel />
           </a>
-          <a href="https//" className={styles.stackCard}>
-            Vercel
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <Vercel />
           </a>
-          <a href="https//" className={styles.stackCard}>
-            Vercel
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <Vercel />
+          </a>
+        </div>
+      </section>
+
+      <section>
+        <h1 className={styles.sectionTitle}>Used in</h1>
+        <div className={styles.container}>
+          <a
+            target="_brank"
+            href="https://ui.shadcn.com/"
+            className={styles.stackCard}
+          >
+            <ShadcnUi />
+          </a>
+          <a
+            target="_brank"
+            href="https://stylelint.io/"
+            className={styles.stackCard}
+          >
+            <Stylelint />
+          </a>
+          <a target="_brank" href="https//" className={styles.stackCard}>
+            <Biome />
           </a>
         </div>
       </section>
